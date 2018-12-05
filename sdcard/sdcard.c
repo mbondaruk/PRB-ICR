95dc99f7d5c19f07cc28eb39ae0c214bc6d45ab2
mbondaru <mbondaru@unix1.csc.calpoly.edu>
2018-12-05 12:25:20 -0800

    spi macros

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,5 +1,31 @@
 #include "sdcard.h"
 
+#define CMD0   (0x40+0) /* GO_IDLE_STATE */
+#define CMD1   (0x40+1) /* SEND_OP_COND (MMC) */
+#define  ACMD41   (0xC0+41)   /* SEND_OP_COND (SDC) */
+#define CMD8   (0x40+8) /* SEND_IF_COND */
+#define CMD9   (0x40+9) /* SEND_CSD */
+#define CMD10  (0x40+10)   /* SEND_CID */
+#define CMD12  (0x40+12)   /* STOP_TRANSMISSION */
+#define ACMD13 (0xC0+13)   /* SD_STATUS (SDC) */
+#define CMD16  (0x40+16)   /* SET_BLOCKLEN */
+#define CMD17  (0x40+17)   /* READ_SINGLE_BLOCK */
+#define CMD18  (0x40+18)   /* READ_MULTIPLE_BLOCK */
+#define CMD23  (0x40+23)   /* SET_BLOCK_COUNT (MMC) */
+#define  ACMD23   (0xC0+23)   /* SET_WR_BLK_ERASE_COUNT (SDC) */
+#define CMD24  (0x40+24)   /* WRITE_BLOCK */
+#define CMD25  (0x40+25)   /* WRITE_MULTIPLE_BLOCK */
+#define CMD55  (0x40+55)   /* APP_CMD */
+#define CMD58  (0x40+58)   /* READ_OCR */
+#define WRITE_SPI(data) {\
+   while(!EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG);\
+   EUSCI_B0->TXBUF = hiByte;\
+}
+#define RECEIVE_SPI() {\
+   while(!EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG);\
+   return EUSCI_B0->RXBUF;
+}
+
 typedef struct descriptor {
    uint32_t CID[4]; //card identification number
    uint16_t RCA; //relative card address
@@ -9,76 +35,94 @@ typedef struct descriptor {
    uint32_t SSR[16]; //sd status register
    uint32_t CSR; //card status register
 } SdCardDescriptor;
-void sdWriteBlock(uint32_t address)
+
+void sdSendCommand(uint8_t cmd, uint32_t arg, uint8_t crc7)
+{
+   WRITE_SPI(cmd);
+   WRITE_SPI(arg>>24);
+   WRITE_SPI(arg>>16);
+   WRITE_SPI(arg>>8);
+   WRITE_SPI(arg);
+   WRITE_SPI(crc7);
+}
+
+void sdSendToken(uint8_t token)
+{
+   WRITE_SPI(token);
+}
+
+void sdWriteBlock(uint32_t address, uint8_t *data, uint8_t crc7)
+{
+   sdSendCommand(CMD24, address, crc7);
+}
+
+void sdWriteMultipleBlocks(uint32_t address, uint8_t crc7)
+{
+   sdSendCommand(CMD25, address, crc7);
+}
+
+void sdReadBlock(uint32_t address, uint8_t *buf, SdCommand* cmd)
+{
+   
+}
+void sdReadMultipleBlocks(uint32_t address, uint8_t *buf, SdCommand* cmd)
 {
 
 }
-void sdWriteMultipleBlocks(uint32_t address)
+void sdProgramCSD(uint32_t stuffbits, SdCommand* cmd)
 {
 
 }
-void sdReadBlock(uint32_t address, uint8_t *buf)
+void sdSetEraseStartAddr(uint32_t address, SdCommand* cmd)
 {
 
 }
-void sdReadMultipleBlocks(uint32_t address, uint8_t *buf)
+void sdSetEraseEndAddr(uint32_t address, SdCommand* cmd)
 {
 
 }
-void sdProgramCSD(uint32_t stuffbits)
+void sdErase(uint32_t stuff, SdCommand* cmd)
 {
 
 }
-void sdSetEraseStartAddr(uint32_t address)
+void sdLockUnlock(SdCommand* cmd)
 {
 
 }
-void sdSetEraseEndAddr(uint32_t address)
+void sdGenCmd(uint32_t rdwr, SdCommand* cmd) //1 - rd, 0 - wr
 {
 
 }
-void sdErase(uint32_t stuff)
+void sdReadOCR(SdCommand* cmd)
 {
 
 }
-void sdLockUnlock()
-{
-
-}
-void sdGenCmd(uint32_t rdwr) //1 - rd, 0 - wr
-{
-
-}
-void sdReadOCR()
-{
-
-}
-void sdCRCOnOff(uint32_t option)
+void sdCRCOnOff(uint32_t option, SdCommand* cmd)
 {
 
 }
 //acmd
-void sdSendStatus()
+void sdSendStatus(SdCommand* cmd)
 {
 
 }
-void sdSendNumWrBlocks()
+void sdSendNumWrBlocks(SdCommand* cmd)
 {
 
 }
-void sdSetWrBlkEraseCnt(uint32_t numBlocks)
+void sdSetWrBlkEraseCnt(uint32_t numBlocks, SdCommand* cmd)
 {
 
 }
-void sdSendOpCond()
+void sdSendOpCond(SdCommand* cmd)
 {
 
 }
-void sdSetClrCardDetect(uint32_t set_cd)
+void sdSetClrCardDetect(uint32_t set_cd, SdCommand* cmd)
 {
 
 }
-void sdSendSCR()
+void sdSendSCR(SdCommand* cmd)
 {
 
 }
